6f1d3f98f047b04832e1dbe9422a53b40cfac685
rster2002 <rster2002dev@gmail.com>
2023-07-14 22:03:56 +0200

    💡 Add comments, so I don't forget how this should work

--- yrs-testing/src/main.rs
+++ yrs-testing/src/main.rs
@@ -8,22 +8,30 @@ fn main() {
     let mut server = ServerFile::new("test");
 
     let mut client_a = ClientFile::new("alice", "test");
+    client_a.use_remote_content(&server.get_op_log()); // Start connection, get ops from server
+
     client_a.insert(0, "Hello");
     client_a.insert(5, " Bob");
 
-    client_a.use_remote_content(&server.get_op_log());
-    server.use_remote_content(&client_a.get_op_log());
+    server.use_remote_content(&client_a.get_op_log()); // Send ops to server
+    client_a.use_remote_content(&server.get_op_log()); // Broadcast ops to clients
 
     let mut client_b = ClientFile::new("bob", "test");
+    client_b.use_remote_content(&server.get_op_log()); // Start connection, get ops from server
+
     client_b.insert(0, "Hello");
     client_b.insert(5, " Alice");
 
-    client_b.use_remote_content(&server.get_op_log());
-    server.use_remote_content(&client_b.get_op_log());
-    client_a.use_remote_content(&server.get_op_log());
+    server.use_remote_content(&client_b.get_op_log()); // Send ops to server
+    client_a.use_remote_content(&server.get_op_log()); // Broadcast ops to clients
+    client_b.use_remote_content(&server.get_op_log()); // Broadcast ops to clients
+
+    let mut client_c = ClientFile::new("celine", "test");
+    client_c.use_remote_content(&server.get_op_log()); // Start connection, get ops from server
 
     dbg!(client_a.get_content());
     dbg!(client_b.get_content());
+    dbg!(client_c.get_content());
 }
 
 struct ServerFile {
